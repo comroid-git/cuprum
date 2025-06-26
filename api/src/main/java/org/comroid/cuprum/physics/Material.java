f7d1febc62b4caeba4606aef4af24b676103bad4
Kaley <kaleidox@comroid.org>
2025-06-26 19:54:41 +0200

    add more materials

--- api/src/main/java/org/comroid/cuprum/physics/Material.java
+++ api/src/main/java/org/comroid/cuprum/physics/Material.java
@@ -10,7 +10,13 @@ import java.awt.*;
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = AccessLevel.PUBLIC)
 public enum Material {
-    @Default COPPER(58, new Color(0xc68346)), ALUMINIUM(37.8, new Color(0xd0d5db));
+    NEGLIGIBLE(0, Color.BLACK),
+    @Default COPPER(58, new Color(0xc68346)),
+    ALUMINIUM(37.8, new Color(0xd0d5db)),
+    GOLD(47.6, new Color(0xc9a752)),
+    IRON(10, new Color(0xbeb9b3)),
+    SILVER(67.1, new Color(0xbfbfbd));
+
     double conductivity;
     Color  color;
 }
