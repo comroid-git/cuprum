e26e1c982fe56d54d276e26c5ff79df5d0d78cd9
Kaley <kaleidox@comroid.org>
2025-07-02 19:33:30 +0200

    fix logic error

--- api/src/main/java/org/comroid/cuprum/component/model/operational/DynamicallyOperated.java
+++ api/src/main/java/org/comroid/cuprum/component/model/operational/DynamicallyOperated.java
@@ -2,6 +2,6 @@ package org.comroid.cuprum.component.model.operational;
 
 public interface DynamicallyOperated extends ManuallyOperated, OperatorChild {
     default boolean isOperated() {
-        return isManuallyOperated() || getParent() == null && getParent().isSufficientlyPowered();
+        return isManuallyOperated() || getParent() != null && getParent().isSufficientlyPowered();
     }
 }
