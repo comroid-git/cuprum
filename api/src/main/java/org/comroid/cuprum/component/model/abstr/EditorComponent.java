082bccc82276fdc4bf35dc9ed045a0a516eda28a
Kaley <kaleidox@comroid.org>
2025-07-02 19:35:27 +0200

    make createRenderObject() abstract

--- api/src/main/java/org/comroid/cuprum/component/model/abstr/EditorComponent.java
+++ api/src/main/java/org/comroid/cuprum/component/model/abstr/EditorComponent.java
@@ -4,12 +4,9 @@ import org.comroid.cuprum.editor.render.RenderObjectAdapter;
 import org.comroid.cuprum.editor.render.UniformRenderObject;
 import org.comroid.cuprum.model.ITransform;
 import org.comroid.cuprum.spatial.Transform;
-import org.jetbrains.annotations.Nullable;
 
 public interface EditorComponent extends CuprumComponent, SnappableComponent, Transform.Holder {
-    default @Nullable UniformRenderObject createRenderObject(RenderObjectAdapter adapter) {
-        return null;
-    }
+    UniformRenderObject createRenderObject(RenderObjectAdapter adapter);
 
     default int priorityLayer() {
         return 0;
